15dfd72e065edf60016847970b292b72e1aa9e82
Lanweijun <582917840@qq.com>
2019-06-25 19:13:47 +0800

    2019-6-25-19-10 butterknife

--- app/src/main/java/com/example/myfirstandroid/MainActivity.java
+++ app/src/main/java/com/example/myfirstandroid/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.myfirstandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+@ViewInject(mianlayoutid = R.layout.activity_main)
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.fac_main_home)
+    FloatingActionButton facMainHome;
+    @BindView(R.id.rg_main_top)
+    RadioGroup rgMainTop;
+    @BindView(R.id.fl_main_content)
+    FrameLayout flMainContent;
+    @BindView(R.id.rb_main_shanghai)
+    RadioButton rbMainShanghai;
+    @BindView(R.id.rb_main_sichuan)
+    RadioButton rbMainSichuan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    }
+
+    @OnClick(R.id.fac_main_home)
+    public void onClick() {
     }
 }
